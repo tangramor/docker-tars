a4b041d99edffae7a28fd53c508bab4af6cada48
tangramor <tangramor@gmail.com>
2018-02-11 17:43:32 +0800

    reorg the file structure

---
+++ tars-node/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ -d /root/init ];then
+
+	for x in $(ls /root/init)
+	do
+		if [ -f /root/init/$x ];then
+			chmod u+x /root/init/$x
+			/bin/bash /root/init/$x
+			rm -rf /root/init/$x
+		fi
+	done
+fi
+
+
+case ${1} in
+	init)
+		;;
+	start)
+		tail -f /var/log/yum.log
+		;;
+	*)
+		exec "$@"
+		;;
+esac
+
